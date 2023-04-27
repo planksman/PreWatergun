d03c1e3098f2ab09bcd9b85a5999f1709fd293b3
Tofi! <52051567+Tofi-Dev@users.noreply.github.com>
2023-04-27 23:09:59 +0200

    Username fixes (#4)

--- scripts/ToMain.gd
+++ scripts/ToMain.gd
@@ -8,6 +8,8 @@ extends Node
 @onready var network_node = get_node("Network")
 @onready var spawnpoints = get_node("Map/SpawnPoints")
 
+var your_fake_name
+
 #func _process(_delta):
 	#if Input.is_action_pressed("ui_cancel"):
 		#get_tree().change_scene_to_file("res://Menu.tscn")
@@ -20,10 +22,10 @@ func _on_host_game_pressed():
 	multiplayer.multiplayer_peer = peer
 	
 	multiplayer.peer_disconnected.connect(remove_player)
-	multiplayer.peer_connected.connect(add_player)
+	#multiplayer.peer_connected.connect(add_player)
 	
 	load_game()
-	add_player(multiplayer.get_unique_id())
+	#add_player(multiplayer.get_unique_id())
 	
 func _on_join_game_pressed():
 	setup_upnp(get_node("%PortJoin").text)
@@ -36,19 +38,22 @@ func _on_join_game_pressed():
 	multiplayer.server_disconnected.connect(server_offline)
 	multiplayer.server_disconnected.connect(kick_everyone)
 	
+	
+func _process(delta):
+	UsernameStuff.current_username = get_node("%Username").text
 
 
 func load_game():
 	%MultiplayerConfig.hide()
-	#var map_inst = map.instantiate()
+	add_player.rpc_id(1,multiplayer.get_unique_id())
 	#map_instance.add_child(map_inst)
 	
-@rpc("any_peer")
+@rpc("any_peer","call_local")
 func add_player(id):
 	print(str(id) + " Has joined the server")
 	var player_instance = player.instantiate()
 	player_instance.name = str(id)
-	player_instance.fake_name = get_node("%Username").text
+	player_instance.fake_name = your_fake_name
 	network_node.add_child(player_instance)
 
 @rpc("any_peer")
