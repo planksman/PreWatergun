38f5ead1d0d47445a2b87d48e1ef9dd2be34c1dc
Planks Wooden <109150161+planksman@users.noreply.github.com>
2023-08-30 22:40:09 +0300

    Update Drip.gd
    Added copyright notice, more license- or/and copyright notices will be added to other files later

--- scripts/Drip.gd
+++ scripts/Drip.gd
@@ -1,3 +1,5 @@
+##Copyright 2023 PlanksMan and Tofi_dev
+
 ##This file is part of PreWatergun.
 
 ##PreWatergun is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
